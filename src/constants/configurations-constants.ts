5aaa6781da4bbdb6a909af9327858c82658391a7
Valerio Laudi <valerio.laudi@codingops.dev>
2022-06-14 14:43:33 +0200

    Created ConfigurationsConstants class

---
+++ src/constants/configurations-constants.ts
@@ -0,0 +1,5 @@
+export default class ConfigurationsConstants {
+    static readonly DEFAULT_AUTHORITY = "https://login.microsoftonline.com/organizations";
+
+    static readonly CACHE_LOCATION_SESSION_STORAGE = "sessionStorage";
+}
