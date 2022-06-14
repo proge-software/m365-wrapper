b6360ea274e53b61652138f48ff7f87bfb8c5398
Valerio Laudi <valerio.laudi@codingops.dev>
2022-06-14 14:43:08 +0200

    Created PopupRequestConstants class

---
+++ src/constants/popup-request-constants.ts
@@ -0,0 +1,9 @@
+export default class PopupRequestConstants {
+    static readonly DEFAULT_SCOPES = ['Calendars.ReadWrite', 'Calendars.Read.Shared',
+    'Directory.AccessAsUser.All', 'Directory.Read.All', 'Directory.ReadWrite.All', 'email',
+    'Files.Read.All', 'Group.Read.All',
+    'OnlineMeetings.ReadWrite', 'Reports.Read.All',
+    'Team.ReadBasic.All', 'User.Read', 'User.Read.All', 'User.ReadBasic.All', 'User.ReadWrite.All'];
+
+    static readonly PROMPT_SELECT_ACCOUNT = "select_account";
+}
