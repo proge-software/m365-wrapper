2c99d2a191dac5563fd4c9a6bfd04e2297a2d5f9
Valerio Laudi <valerio.laudi@codingops.dev>
2022-06-15 18:05:10 +0200

    Implemented M365WrapperDataResult model in the UserHandler class

--- src/handlers/user-handler.ts
+++ src/handlers/user-handler.ts
@@ -2,6 +2,9 @@ import { PublicClientApplication, AuthenticationResult, AccountInfo, PopupReques
 import { Client } from "@microsoft/microsoft-graph-client";
 import * as MicrosoftGraph from '@microsoft/microsoft-graph-types';
 import PopupRequestConstants from "../constants/popup-request-constants";
+import M365WrapperDataResult from "../models/results/m365-wrapper-data-result";
+import M365WrapperResult from "../models/results/m365-wrapper-result";
+import ErrorsHandler from "./errors-handler";
 
 export default class UserHandler {
 
@@ -12,88 +15,129 @@ export default class UserHandler {
 
     constructor(private readonly msalApplication: PublicClientApplication, private readonly client: Client) { }
 
-    public async loginPopup(): Promise<AuthenticationResult> {
-        let loginResult = await this.msalApplication.loginPopup(this.popupRequest);
-        this.msalApplication.setActiveAccount(loginResult.account);
-        return loginResult;
+    public async loginPopup(): Promise<M365WrapperDataResult<AuthenticationResult>> {
+
+        try {
+            let loginResult = await this.msalApplication.loginPopup(this.popupRequest);
+            this.msalApplication.setActiveAccount(loginResult.account);
+            return M365WrapperDataResult.createSuccess(loginResult);
+        }
+        catch (error) {
+            return ErrorsHandler.getErrorDataResult(error);
+        }
     }
 
-    public async acquireTokenSilent(): Promise<AuthenticationResult> {
-        return await this.msalApplication.acquireTokenSilent(this.popupRequest);
+    public async acquireTokenSilent(): Promise<M365WrapperDataResult<AuthenticationResult>> {
+
+        try {
+            return M365WrapperDataResult.createSuccess(await this.msalApplication.acquireTokenSilent(this.popupRequest));
+        }
+        catch (error) {
+            return ErrorsHandler.getErrorDataResult(error);
+        }
     }
 
-    public async acquireTokenPopup(): Promise<AuthenticationResult> {
-        return await this.msalApplication.acquireTokenPopup(this.popupRequest);
+    public async acquireTokenPopup(): Promise<M365WrapperDataResult<AuthenticationResult>> {
+
+        try {
+            return M365WrapperDataResult.createSuccess(await this.msalApplication.acquireTokenPopup(this.popupRequest));
+        }
+        catch (error) {
+            return ErrorsHandler.getErrorDataResult(error);
+        }
     }
 
     public async statLoginPopupProcess() {
-        let account = this.getMyAccountInfo();
-        if (account) {
-            await this.acquireTokenSilent().then(async response => {
-                //const account = thatMsal.getAccount();
-                // this.SET_ACCOUNT(account);
-                // this.SET_ID_TOKEN(response);
-                // this.SET_LOGIN_STATE(true);       
-            }).catch(async error => {
-                if (error.errorMessage.indexOf("interaction_required") !== -1) {
-                    await this.acquireTokenPopup()
-                        .then(async response => {
-                            const account = this.getMyAccountInfo();
+
+        try {
+            let account = this.getMyAccountInfo();
+            if (account) {
+                await this.acquireTokenSilent().then(async response => {
+                    //const account = thatMsal.getAccount();
+                    // this.SET_ACCOUNT(account);
+                    // this.SET_ID_TOKEN(response);
+                    // this.SET_LOGIN_STATE(true);       
+                }).catch(async error => {
+                    if (error.errorMessage.indexOf("interaction_required") !== -1) {
+                        await this.acquireTokenPopup()
+                            .then(async response => {
+                                let account = this.getMyAccountInfo();
+                                // this.SET_ACCOUNT(account);
+                                // this.SET_ID_TOKEN(response);
+                                // this.SET_LOGIN_STATE(true);              
+                            })
+                            .catch(err => {
+                                return ErrorsHandler.getErrorDataResult(err);
+                            });
+                    }
+                    else
+                        return ErrorsHandler.getErrorDataResult(error);
+                })
+            }
+            else {
+                await this.loginPopup()
+                    .then(async response => {
+                        await this.acquireTokenSilent().then(async response => {
+                            account = this.getMyAccountInfo();
                             // this.SET_ACCOUNT(account);
                             // this.SET_ID_TOKEN(response);
-                            // this.SET_LOGIN_STATE(true);              
+                            // this.SET_LOGIN_STATE(true);            
                         })
-                        .catch(err => {
-                            console.log(err);
-                        });
-                }
-                else
-                    console.log(error);
-            })
-        }
-        else {
-            await this.loginPopup()
-                .then(async response => {
-                    await this.acquireTokenSilent().then(async response => {
-                        account = this.getMyAccountInfo();
-                        // this.SET_ACCOUNT(account);
-                        // this.SET_ID_TOKEN(response);
-                        // this.SET_LOGIN_STATE(true);            
+                            .catch(err => {
+                                return ErrorsHandler.getErrorDataResult(err);
+                            });
                     })
-                        .catch(err => {
-                            console.log(err);
-                        });
-                })
-                .catch(err => {
-                    console.log(err);
-                });
+                    .catch(err => {
+                        return ErrorsHandler.getErrorDataResult(err);
+                    });
 
+            }
+        }
+        catch (error) {
+            return ErrorsHandler.getErrorDataResult(error);
         }
     }
 
-    public getMyAccountInfo(): AccountInfo {
-        return this.msalApplication.getActiveAccount();
+    public getMyAccountInfo(): M365WrapperDataResult<AccountInfo> {
+        try {
+            return M365WrapperDataResult.createSuccess(this.msalApplication.getActiveAccount());
+        }
+        catch (error) {
+            return ErrorsHandler.getErrorDataResult(error);
+        }
     }
 
-    public logoutPopup() {
-        this.msalApplication.logoutPopup(this.popupRequest);
+    public async logoutPopup(): Promise<M365WrapperResult> {
+        try {
+            await this.msalApplication.logoutPopup(this.popupRequest);
+            return new M365WrapperResult();
+        }
+        catch (error) {
+            return ErrorsHandler.getErrorDataResult(error);
+        }
     }
 
-    public async getMyDetails(): Promise<MicrosoftGraph.User> {
-        return (await this.client.api("/me").get()) as MicrosoftGraph.User;
+    public async getMyDetails(): Promise<M365WrapperDataResult<MicrosoftGraph.User>> {
+        try {
+            let result: MicrosoftGraph.User = await this.client.api("/me").get();
+            return M365WrapperDataResult.createSuccess(result);
+        }
+        catch (error) {
+            return ErrorsHandler.getErrorDataResult(error);
+        }
     }
 
     // GetMyApplications: Permissions problems (output 403: Forbidden)
-    public async getMyApplications(): Promise<any> {
+    public async getMyApplications(): Promise<M365WrapperDataResult<any>> {
         try {
             // const retReport = await this.client.api("/reports/getOffice365ActivationsUserDetail(period='D7')")
             // const retReport = await this.client.api("/reports/getOffice365ActivationsUserDetail")
-            const retReport = await this.client.api("/reports/getOffice365ActiveUserDetail(period='D7')")
+            let retReport = await this.client.api("/reports/getOffice365ActiveUserDetail(period='D7')")
                 .get();
-            return retReport;
+            return M365WrapperDataResult.createSuccess(retReport);
         }
         catch (error) {
-            throw error;
+            return ErrorsHandler.getErrorDataResult(error);
         }
     }
 }
