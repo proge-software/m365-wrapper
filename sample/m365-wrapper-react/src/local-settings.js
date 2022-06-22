d26cd8ab5fa864c1bc222df4eb0388663d1da0de
Valerio Laudi <valerio.laudi@codingops.dev>
2022-06-22 16:06:41 +0200

    [m365-wrapper-react] Removed application id

--- sample/m365-wrapper-react/src/local-settings.js
+++ sample/m365-wrapper-react/src/local-settings.js
@@ -1,3 +1,3 @@
 export class LocalSettings {
-    static APPLICATION_ID = "23a3e579-e95e-4c61-bf38-6d538246db84"; // Set your Azure Application Id
+    static APPLICATION_ID = "{APP_ID}"; // Set your Azure Application Id
 }
