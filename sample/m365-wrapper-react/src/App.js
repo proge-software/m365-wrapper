8d671be186f78abe79aac0f23fabf265dabf695c
Valerio Laudi <valerio.laudi@codingops.dev>
2022-06-22 15:57:54 +0200

    [m365-wrapper-react] Created Settings class for manage the configurations

--- sample/m365-wrapper-react/src/App.js
+++ sample/m365-wrapper-react/src/App.js
@@ -1,11 +1,12 @@
 import M365Wrapper from '../node_modules/m365-wrapper/lib/index';
 import React from 'react';
 import './App.css';
+import { Settings } from './settings';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { userInfo: undefined, m365wrapper: new M365Wrapper('{APP ID}') };
+    this.state = { userInfo: undefined, m365wrapper: new M365Wrapper(Settings.APPLICATION_ID) };
   }
 
   async componentDidMount() {
---
+++ sample/m365-wrapper-react/src/settings.js
@@ -0,0 +1,3 @@
+export class Settings {
+    static APPLICATION_ID = "{APP_ID}"; // Set your Azure Application Id
+}
